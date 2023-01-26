1f0e6f877541da94ed7a94398843bfcfad117515
Matheus Oliveira <matheus_luiz2011@hotmail.com>
2023-01-25 23:04:50 -0300

    7- Coding challenge

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,19 +13,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+        val countButton: Button = findViewById(R.id.count_button)
 
         rollButton.setOnClickListener{rollDice()}
 
+        countButton.setOnClickListener{countUp()}
 
-        }
+
+    }
     private fun rollDice(){
 //        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
-
-        val resultText : TextView  = findViewById(R.id.result_text)
+        val resultText : TextView = findViewById(R.id.result_text)
 
         resultText.text = randomInt.toString()
+    }
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val strText = resultText.text.toString()
 
-
+        if (strText != "Hello World!") {
+            var intText = strText.toInt()
+            intText += 1
+            if (intText <= 6) {
+                resultText.text = intText.toString()
+            }
+        }
     }
 }
